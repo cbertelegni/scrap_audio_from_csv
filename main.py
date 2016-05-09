53d17cd3a016ddc868ec7b475bca64c5ac311508
cbertelegni <cristianbertelegni@gmail.com>
2016-05-09 15:20:07 -0300

    Closes #1

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import os, csv, urllib2
+from time import sleep
+from scrapfromcsv.scrapfromcsv import Scrapfromcsv
 
 BASE  = os.path.dirname(__file__)
 
@@ -14,40 +16,9 @@ column_src = 0 # column src audio on csv
 INPUT_SRC = os.path.join(BASE, audio_csv);
 OUTPUT_DIR = os.path.join(BASE, "output");
 
-
-
 """ Scrap audios """
 if __name__ == "__main__":
 
-    """ check if OUTPUT_DIR exists"""
-    if not os.path.exists(OUTPUT_DIR):
-        os.makedirs(OUTPUT_DIR)
+   scraper = Scrapfromcsv(INPUT_SRC, column_src, OUTPUT_DIR)
 
-    """ load source audios """
-    csv_file = csv.reader(open(INPUT_SRC, "rb"))
-
-    for row in csv_file:
-        src =  row[column_src]
-        file_name = src.split('/')[-1]
-
-        if file_name:
-            f_path = os.path.join(OUTPUT_DIR, file_name)
-
-            if not os.path.exists(f_path):
-                req = urllib2.Request(src)
-                try:
-                    req = urllib2.Request(src)
-
-                    """ uncomment you need set the referer"""
-                    # req.add_header('Referer', 'http://www.example.com/')
-                    # req.add_header('User-agent', 'Mozilla/5.0')
-                    r = urllib2.urlopen(req)
-
-                    with open(f_path,'wb') as output:
-                      output.write(r.read())
-
-                except:
-                    print "%s --> Error!" % f_path
-            else:
-                print "%s --> Exists!" % f_path
-                
+   scraper.scrap(referer_url = "http://example.com")
---
+++ scrapfromcsv/scrapfromcsv.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os, csv, urllib2
+from time import sleep
+
+class Scrapfromcsv:
+    def __init__(self, csv_file, column_src, output_dir):
+
+        self.csv_file = csv_file
+        self.column_src = column_src
+        self.output_dir = output_dir
+
+
+    def scrap(self, referer_url = None):
+
+        # print self.OUTPUT_DIR
+        """ check if OUTPUT_DIR exists"""
+        if not os.path.exists(self.output_dir):
+            os.makedirs(self.output_dir)
+
+        """ load source audios """
+        csv_file = csv.reader(open(self.csv_file, "rb"))
+
+        for row in csv_file:
+            src =  row[self.column_src]
+            file_name = src.split('/')[-1]
+
+            if file_name:
+                f_path = os.path.join(self.output_dir, file_name)
+
+                if not os.path.exists(f_path):
+                    req = urllib2.Request(src)
+                    try:
+                        req = urllib2.Request(src)
+                        req.add_header('User-agent', 'Mozilla/5.0')
+
+                        """ set referer for scrap """
+                        if referer_url:
+                            req.add_header('Referer', referer_url)
+                        r = urllib2.urlopen(req)
+
+                        with open(f_path,'wb') as output:
+                            output.write(r.read())
+                            print "saved %s \n" % file_name
+                            
+                            sleep(1)
+                          
+
+                    except Exception, e:
+                        print "%s --> Error: %s" % (f_path, e)
+                else:
+                    print "%s --> Exists!" % f_path
