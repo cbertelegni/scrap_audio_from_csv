ec74082c19b722cd98df4ae8c440517e63583b9e
Cristian Bertelegni <cristianbertelegni@gmail.com>
2016-05-09 15:41:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,16 +11,15 @@ BASE  = os.path.dirname(__file__)
 # column_src = 1 # column src audio on csv
 audio_csv = "src.csv"
 column_src = 0 # column src audio on csv
-
-
-INPUT_SRC = os.path.join(BASE, audio_csv);
-OUTPUT_DIR = os.path.join(BASE, "output");
+output = "output" # name of the output dir
 # referer_url = "http://example.com"
 referer_url = None
 
+
 """ Scrap audios """
 if __name__ == "__main__":
-
+   INPUT_SRC = os.path.join(BASE, audio_csv);
+   OUTPUT_DIR = os.path.join(BASE, output);
+   
    scraper = Scrapfromcsv(INPUT_SRC, column_src, OUTPUT_DIR, referer_url)
-
    scraper.scrap()
